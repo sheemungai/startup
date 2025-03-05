ea28cc448ffbb3cf85b48d01bed0e76cfa45e497
sheilla mungai <mungaisheilla19@gmail.com>
2025-03-05 22:43:30 +0300

    added features

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 //initialize swiper instance
-const swiper = new swiper(".slider-container",{
-    effect:"fade",
-    speed:1300,
-})
+document.addEventListener("DOMContentLoaded", function () {
+    var swiper = new Swiper(".swiper", {
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
+});
